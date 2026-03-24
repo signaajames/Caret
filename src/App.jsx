d10591d9e5176bb4873d53511ab4629bf9b275ce
signaajames <penguingnop@gmail.com>
2026-03-24 09:57:59 +0800

    monaco editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import Editor from "@monaco-editor/react";
 
 function App() {
   return (
-  <div></div> 
+  <div className="shell">
+    <Editor
+      height="100%"
+      width="100%"
+    />
+  </div>
   );
 }
 
